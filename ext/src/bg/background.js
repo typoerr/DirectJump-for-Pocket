8fa6a5dfcb9349df1f4fd767fa811a427de435e1
cotto89 <cottto89@gmail.com>
2016-02-05 14:46:43 +0900

    background.jsからURLの推移(tabの更新)を検知してContentScriptにsendMessage

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,1 +1,8 @@
-//TODO: URLの推移(tabの更新)を検知してContentScriptにmessage passing
+"use strict";
+
+// URLの推移(tabの更新)を検知してPocketの場合にContentScriptにmessage passing
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (tab.url.match(/https:\/\/getpocket.com\//) && changeInfo.status === 'complete') {
+    chrome.tabs.sendMessage(tabId, {type: 'RUN'})
+  }
+});
