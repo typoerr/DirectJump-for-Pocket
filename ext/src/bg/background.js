bbf4a1ca1138b8b2eaa9c7eefc58377cdeff9703
cotto89 <cottto89@gmail.com>
2016-02-05 19:52:37 +0900

    即時関数でラップ

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,8 +1,11 @@
-"use strict";
+(function () {
+  "use strict";
 
-// URLの推移(tabの更新)を検知してPocketの場合にContentScriptにmessage passing
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (tab.url.match(/https:\/\/getpocket.com\//) && changeInfo.status === 'complete') {
-    chrome.tabs.sendMessage(tabId, {type: 'RUN'})
-  }
-});
+  // URLの推移(tabの更新)を検知してPocketの場合にContentScriptにmessage passing
+  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (tab.url.match(/https:\/\/getpocket.com\//) && changeInfo.status === 'complete') {
+      chrome.tabs.sendMessage(tabId, { type: 'RUN' })
+    }
+  });
+
+})();
--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,59 +1,61 @@
-"use strict";
-chrome.runtime.onMessage.addListener(function (req) {
-  switch (req.type) {
-    case "RUN":
-      setTimeout(function () {
-        run()
-      }, 700);
-      break;
-    default:
-      console.log("Error: Unknown request. : ", req);
-  }
-});
-
-
-const run = () => {
-  let originalUrls = getOriginalUrls();
-  let $item_links = document.getElementsByClassName('item_link');
-
-  Array.prototype.forEach.call($item_links, function (elm, idx) {
-
-    //.item_linkのhrefを書き換える
-    elm.href = originalUrls[idx];
-
-    // 他のイベントリスナの実行を中止する
-    elm.addEventListener('click', function (e) {
-      e.stopImmediatePropagation();
-    })
-  });
-};
-
-// '.original_url'を取得してurlの配列を返す
-const getOriginalUrls = () => {
-  let targetElms = Array.prototype.slice.call(document.getElementsByClassName('original_url'));
-  return targetElms.map((item) => item.href);
-};
-
-
-/*------------------------
-* AutoScrollに対応
-* ------------------------
-* Scrollイベントでbodyの高さを監視して、高さが増えるとrun()を再実行する
-* */
-function ScrollWatcher() {
-  this.bodyHeight = document.body.offsetHeight;
-}
-
-ScrollWatcher.prototype.watch = function () {
-  window.addEventListener('scroll', () => {
-    let _bodyHeight = document.body.offsetHeight;
-
-    if (this.bodyHeight !== _bodyHeight) {
-      run();
-      this.bodyHeight = _bodyHeight;
+(function () {
+  "use strict";
+  chrome.runtime.onMessage.addListener(function (req) {
+    switch (req.type) {
+      case "RUN":
+        setTimeout(function () {
+          run()
+        }, 700);
+        break;
+      default:
+        console.log("Error: Unknown request. : ", req);
     }
   });
-};
 
-const scrollWatcher = new ScrollWatcher();
-scrollWatcher.watch();
+
+  const run = () => {
+    let originalUrls = getOriginalUrls();
+    let $item_links = document.getElementsByClassName('item_link');
+
+    Array.prototype.forEach.call($item_links, function (elm, idx) {
+
+      //.item_linkのhrefを書き換える
+      elm.href = originalUrls[idx];
+
+      // 他のイベントリスナの実行を中止する
+      elm.addEventListener('click', function (e) {
+        e.stopImmediatePropagation();
+      })
+    });
+  };
+
+// '.original_url'を取得してurlの配列を返す
+  const getOriginalUrls = () => {
+    let targetElms = Array.prototype.slice.call(document.getElementsByClassName('original_url'));
+    return targetElms.map((item) => item.href);
+  };
+
+
+  /*------------------------
+  * AutoScrollに対応
+  * ------------------------
+  * Scrollイベントでbodyの高さを監視して、高さが増えるとrun()を再実行する
+  * */
+  function ScrollWatcher() {
+    this.bodyHeight = document.body.offsetHeight;
+  }
+
+  ScrollWatcher.prototype.watch = function () {
+    window.addEventListener('scroll', () => {
+      let _bodyHeight = document.body.offsetHeight;
+
+      if (this.bodyHeight !== _bodyHeight) {
+        run();
+        this.bodyHeight = _bodyHeight;
+      }
+    });
+  };
+
+  const scrollWatcher = new ScrollWatcher();
+  scrollWatcher.watch();
+})();
