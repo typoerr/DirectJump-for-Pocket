276811ed1d6fb6547fc8af34a71cb12d83d3c64c
cotto89 <cottto89@gmail.com>
2016-02-05 14:54:48 +0900

    inject.jsでbackground.jsからmessageを受け取り

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -4,3 +4,20 @@
 //TODO: item_content数分のoriginal_urlを取得
 //TODO: item_contentのitem_linkを書き換える
 //TODO: item_linkのclickイベントを奪取
+
+chrome.runtime.onMessage.addListener(function (req) {
+  switch (req.type) {
+    case "RUN":
+      console.log('発火');
+      run();
+      break;
+    default:
+      console.log("Error: Unknown request. : ", req);
+  }
+});
+
+const run = () => {
+  let urls = document.getElementsByClassName('original_url');
+  console.log(urls[0].href);
+
+};
