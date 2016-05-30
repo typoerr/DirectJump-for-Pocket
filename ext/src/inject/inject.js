3df2293653a9c326790f8a60b990b6ce0d969c53
cotto89 <cottto89@gmail.com>
2016-05-30 11:02:25 +0900

    [clean]リファクタリング

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,39 +1,38 @@
 (function () {
   "use strict";
+
+  const run = () => {
+    setTimeout(() => {
+      const originalUrls = getOriginalUrls();
+      const $item_links = document.getElementsByClassName('item_link');
+
+      Array.prototype.forEach.call($item_links, function (elm, idx) {
+        elm.href = originalUrls[idx];
+        elm.addEventListener('click', function (e) {
+          e.stopImmediatePropagation();
+        })
+      });
+    }, 0);
+  };
+
+  // '.original_url'を取得してurlの配列を返す
+  const getOriginalUrls = () => {
+    let targetElms = Array.prototype.slice.call(document.getElementsByClassName('original_url'));
+    return targetElms.map(item => item.href);
+  };
+
   chrome.runtime.onMessage.addListener(function (req) {
     switch (req.type) {
       case "RUN":
         setTimeout(function () {
           run()
-        }, 700);
+        }, 400);
         break;
       default:
         console.log("Error: Unknown request. : ", req);
     }
   });
 
-
-  const run = () => {
-    let originalUrls = getOriginalUrls();
-    let $item_links = document.getElementsByClassName('item_link');
-
-    Array.prototype.forEach.call($item_links, function (elm, idx) {
-
-      //.item_linkのhrefを書き換える
-      elm.href = originalUrls[idx];
-
-      // 他のイベントリスナの実行を中止する
-      elm.addEventListener('click', function (e) {
-        e.stopImmediatePropagation();
-      })
-    });
-  };
-
-// '.original_url'を取得してurlの配列を返す
-  const getOriginalUrls = () => {
-    let targetElms = Array.prototype.slice.call(document.getElementsByClassName('original_url'));
-    return targetElms.map((item) => item.href);
-  };
 
 
   /*------------------------
@@ -47,7 +46,7 @@
 
   ScrollWatcher.prototype.watch = function () {
     window.addEventListener('scroll', () => {
-      let _bodyHeight = document.body.offsetHeight;
+      const _bodyHeight = document.body.offsetHeight;
 
       if (this.bodyHeight !== _bodyHeight) {
         run();
